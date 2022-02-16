443b2c543bd074a6c007060247da785060b502aa
Joshua Cordoba <jcordobab101@icloud.com>
2022-02-16 05:13:17 -0600

    updating repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-/*!
-* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
+// Scripts for sideNav and toggler
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
@@ -23,7 +17,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItems.map(function(responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
